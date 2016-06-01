fcc1958e1645f245b3fe0dd885637b9f06f3c001
Jiun Wei Chia <jiunwei@gmail.com>
2016-06-01 10:25:48 +0800

    Add PyAutoGUI

--- py3installers/install.bat
+++ py3installers/install.bat
@@ -1,5 +1,5 @@
 set targetdir=%ProgramFiles%\Python35
-set pipargs=install --no-index -f .
-python-3.5.1-amd64 /passive InstallAllUsers=1 PrependPath=1 DefaultAllUsersTargetDir="%targetdir%"
+set pipargs=install --no-index -f %~dp0
+python-3.5.1-amd64 /quiet InstallAllUsers=1 PrependPath=1 DefaultAllUsersTargetDir="%targetdir%"
 "%targetdir%\python" -m pip %pipargs% -U pip
 "%targetdir%\Scripts\pip" %pipargs% -r requirements.txt
---
+++ py3testers/test_pyautogui.py
@@ -0,0 +1,3 @@
+import pyautogui
+pyautogui.alert('This is an alert box.')
+pyautogui.typewrite(['win', 'i', 'd', 'l', 'e', 'enter'], interval=0.2)
