82bb100046ddcac50af635f1d30ae527444d10ef
Jiun Wei Chia <jiunwei@gmail.com>
2016-06-28 11:27:59 +0800

    Simplify and add pauses

---
+++ install-amd64.cmd
@@ -0,0 +1,10 @@
+"%~dp0python-3.5.1-amd64.exe" /quiet /uninstall
+ping 127.0.0.1 -n 20
+"%~dp0python-3.5.1-amd64.exe" /quiet InstallAllUsers=1 PrependPath=1 DefaultAllUsersTargetDir="%ProgramFiles%\Python35"
+ping 127.0.0.1 -n 20
+"%ProgramFiles%\Python35\python.exe" -m pip install --no-index -f "%~dp0." -U pip
+ping 127.0.0.1 -n 20
+"%ProgramFiles%\Python35\Scripts\pip.exe" install --no-index -f "%~dp0." -r "%~dp0requirements.txt"
+ping 127.0.0.1 -n 20
+"%ProgramFiles%\Python35\Scripts\pip.exe" install --no-index -f "%~dp0." -r "%~dp0requirements-workaround.txt"
+ping 127.0.0.1 -n 20
