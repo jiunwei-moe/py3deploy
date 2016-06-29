d565328e7f70630209dda00031a64c5698a9955c
Jiun Wei Chia <jiunwei@gmail.com>
2016-06-29 14:20:47 +0800

    Upgrade to 3.5.2

--- install-amd64-nowait.cmd
+++ install-amd64-nowait.cmd
@@ -1,5 +1,5 @@
-"%~dp0python-3.5.1-amd64.exe" /quiet /uninstall
-"%~dp0python-3.5.1-amd64.exe" /quiet InstallAllUsers=1 PrependPath=1 DefaultAllUsersTargetDir="%ProgramFiles%\Python35"
+"%~dp0python-3.5.2-amd64.exe" /quiet /uninstall
+"%~dp0python-3.5.2-amd64.exe" /quiet InstallAllUsers=1 PrependPath=1 DefaultAllUsersTargetDir="%ProgramFiles%\Python35"
 "%ProgramFiles%\Python35\python.exe" -m pip install --no-index -f "%~dp0." -U pip
 "%ProgramFiles%\Python35\Scripts\pip.exe" install --no-index -f "%~dp0." -r "%~dp0requirements.txt"
 "%ProgramFiles%\Python35\Scripts\pip.exe" install --no-index -f "%~dp0." -r "%~dp0requirements-workaround.txt"
